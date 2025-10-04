4d92b76041627cf71abbccc41c227b296f98e90a
federicodececco <federicodececco1@gmail.com>
2025-10-04 14:09:43 +0200

    feat: added other informations section

--- src/app/chef/chef-registration/page.tsx
+++ src/app/chef/chef-registration/page.tsx
@@ -3,8 +3,12 @@ import ChefCarousel from "@/components/ChefCarousel";
 import CitySlider from "@/components/CitySlider";
 import {
   CalendarDays,
+  File,
+  Globe,
   MessagesSquare,
+  Phone,
   ReceiptText,
+  Shield,
   SlidersVertical,
 } from "lucide-react";
 
@@ -206,7 +210,7 @@ export default function ChefRegistration() {
         </section>
 
         {/* chefs section */}
-        <section className="mx-auto w-full px-3 md:flex md:items-center md:gap-8">
+        <section className="mx-auto w-full px-3 pb-10 md:flex md:items-center md:gap-8">
           <div className="order-1 pt-10 pb-4 text-left md:w-1/3 md:flex-shrink-0 md:pl-20 lg:pl-25 xl:pl-40 2xl:pl-45">
             <h1 className="text-4xl">Hanno già fatto il passo</h1>
             <div className="hidden py-8 md:block">
@@ -230,9 +234,52 @@ export default function ChefRegistration() {
         </section>
 
         {/* other informations section */}
-        <section className="w-full bg-[#232323] py-20 md:px-20">
-          <div className="bg-[#0A0A0A]"></div>
+        <section className="w-full bg-[#232323] py-10 md:px-20">
+          <div className="rounded-xl bg-[#0A0A0A] py-15">
+            <div className="mx-auto max-w-2xl pb-8 text-center md:text-left">
+              <h1 className="pb-5 text-4xl">
+                Siamo lì quando hai più bisgono di noi
+              </h1>
+              <h2 className="text-white/70">
+                Non preoccuparti e affidati al nostro team, attivo 24 ore su 24,
+                7 giorni su 7, per aiutarti in ogni momento.
+              </h2>
+            </div>
+            <div className="mx-auto flex max-w-2xl flex-wrap rounded-xl bg-[#232323] p-4">
+              <div className="w-1/2 p-3">
+                <Shield color="#c8a36a" size={30} />
+                <h1 className="pt-4">Pagamenti sicuri</h1>
+                <h2 className="text-white/70">
+                  Tutti i nostri Pagamenti sono crittografati per proteggere i
+                  tuoi dati
+                </h2>
+              </div>
+              <div className="w-1/2 p-3">
+                <Globe color="#c8a36a" size={30} />
+                <h1 className="pt-4">Assicurazione globale</h1>
+                <h2 className="text-white/70">
+                  Copriamo qualsiasi incidente durante l'esperienza.
+                </h2>
+              </div>
+              <div className="w-1/2 p-3">
+                <File color="#c8a36a" size={30} />
+                <h1 className="pt-4">Politica di cancellazione flessibile</h1>
+                <h2 className="text-white/70">
+                  Flessibilità quando più ne hai bisogno.
+                </h2>
+              </div>
+              <div className="w-1/2 p-3">
+                <Phone color="#c8a36a" size={30} />
+                <h1 className="pt-4">Supporto 24 ore su 24, 7 giorni su 7</h1>
+                <h2 className="text-white/70">
+                  Ti risponderemo in meno di 60 secondi.
+                </h2>
+              </div>
+            </div>
+          </div>
         </section>
+
+        <section></section>
       </div>
     </>
   );

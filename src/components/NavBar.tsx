01a78ff574bb1181a5fb842c7be664f98c6975fc
federicodececco <federicodececco1@gmail.com>
2025-10-07 19:30:58 +0200

    fixed: navbar style

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,8 @@ import { useRouter } from "next/navigation";
 
 export default function NavBar() {
   const [isAuth, setIsAuth] = useState(false);
-
+  const [isChef, setIsChef] = useState(false);
+  const [chefSlug, setChefSlug] = useState("");
   const router = useRouter();
 
   const fetchUser = async () => {
@@ -15,6 +16,10 @@ export default function NavBar() {
       const res = await axiosIstance.get("/auth/user");
       if (!res) {
         return;
+      }
+      if (res.data.user.chef) {
+        setIsChef(true);
+        setChefSlug(`${res.data.user.chef.id}-${res.data.user.chef.slug}`);
       }
       console.log(res);
       setIsAuth(true);
@@ -25,7 +30,7 @@ export default function NavBar() {
   const handleLogout = async () => {
     try {
       await axiosIstance.post("/auth/logout");
-      window.location.reload();
+      window.location.replace(`/chef/chef-registration`);
     } catch (error) {
       console.log(error);
     }
@@ -45,7 +50,7 @@ export default function NavBar() {
   return (
     <nav className="bg-first-theme absolute -top-18 z-12 grid h-18 w-full grid-cols-12 text-center text-white">
       Sono una navbar
-      <div className="col-start-11 flex items-center justify-center">
+      <div className="col-start-10 flex items-center justify-center">
         {!isAuth && (
           <button
             onClick={() => {
@@ -56,14 +61,38 @@ export default function NavBar() {
             Login
           </button>
         )}
-
         {isAuth && (
           <button
+            className="outline-gold bg-gold hover:text-gold rounded-xl px-6 py-3 duration-200 hover:cursor-pointer hover:bg-transparent hover:outline"
             onClick={() => {
               handleLogout();
             }}
           >
             Logout
+          </button>
+        )}
+      </div>
+      <div className="flex items-center justify-center">
+        {isChef && (
+          <button
+            className="outline-gold bg-gold hover:text-gold rounded-xl px-6 py-3 duration-200 hover:cursor-pointer hover:bg-transparent hover:outline"
+            onClick={() => {
+              router.push(`/chef/dashboard`);
+            }}
+          >
+            Area personale
+          </button>
+        )}
+      </div>
+      <div className="flex items-center justify-center">
+        {isChef && (
+          <button
+            className="outline-gold bg-gold hover:text-gold rounded-xl px-6 py-3 duration-200 hover:cursor-pointer hover:bg-transparent hover:outline"
+            onClick={() => {
+              router.push(`/chef/${chefSlug}`);
+            }}
+          >
+            La mia pagina
           </button>
         )}
       </div>
